0a0886e0b200b5d0e1d41df34d4ed8993cda609b
EmanMK <84907456+EmanMK@users.noreply.github.com>
2021-11-22 18:56:00 +0200

    Update RideDetails.java

--- RideDetails.java
+++ RideDetails.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package drivingsystem;
 
-/**
- *
- * @author DELL
- */
+import java.util.ArrayList;
+
 public class RideDetails {
-    
+	private int rideId ;
+	private RegularUser User ;
+	private Driver  driver;
+	private String source ;
+	private String destnation ;
+	private ArrayList<Offer> suggestedPrices;
+	private float price;
+	public DB database;
+	
+	
+	public void listSuggested() {
+		
+	}
+	public void addPrice(float price, int driverID) {
+	
+	}
 }
